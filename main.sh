8f9639f976046cd580a32b1b99fa95534adec11e
Alexander Stefani <al3xand3rst3fani@gmail.com>
2020-06-08 23:07:57 +0200

    Small improvements

--- main.sh
+++ main.sh
@@ -4,22 +4,22 @@
 fstep=$3
 echo "Freq vec:  [100 :" $fstep ": 500] Hz"
 freq=()
-for i in $(seq 100 $fstep 500); do freq+=($i); done
-fstr=$( IFS=$' '; echo "${freq[*]}" )
+for i in $(seq 100 $fstep 500);do freq+=($i); done
+fstr=$(IFS=$' '; echo "${freq[*]}")
 sed "1s/@/$fstr/" tlumik/case.txt > tlumik/case.sif
-number=$( echo "400/$fstep + 1" | bc)
+number=$(echo "400/$fstep + 1" | bc)
 sed -i "15s/@/$number/" tlumik/case.sif
 rm -f tlumik/*.dat plaski/*.dat
 cp tlumik/case.sif plaski/case.sif
 # wykonaj plan eksperymentu: 
-echo "Calc for:   x1  |  x2  |  x-coords"
+echo "Calc for:   x1  |  x2  |  x-coords (meters)"
 for x1 in $(seq 0.05 $1 0.4);do
 	for x2 in $(seq 0.2 $2 1);do
 		if [ $(echo "$x2 > 2*$x1" | bc) -eq 1 ];then
 			# obliczenia dla tlumika:
 			octave --silent --eval "replace($x1,$x2)"
-			xline=$( cat xstr.txt )
-			echo "          " $x1 " " $x2 "   " $xline
+			xline=$(cat xstr.txt)
+			echo "          " $x1 "|" $x2 " | " $xline
 			sed "4s/@/$xline/" tlumik.txt > tlumik.grd
 			ElmerGrid 1 2 tlumik.grd > /dev/null
 			cd tlumik
