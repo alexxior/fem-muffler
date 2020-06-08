fc3f3d08ee0c3829c6228e38d88d8eb09e29046d
Alexander Stefani <al3xand3rst3fani@gmail.com>
2020-06-08 19:21:09 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 fstep=$3
-echo Freq vec from 100 to 500 Hz with step $fstep Hz
-cd tlumik
-rm -f *.vtu *.dat
-cd ..
+echo "Freq vec:  [100 :" $fstep ": 500] Hz"
+rm -f tlumik/*.vtu tlumik/*.dat
+echo "Calc for:   x1  |  x2  |  x-coords"
 for x1 in $(seq 0.05 $1 0.4);do
 	for x2 in $(seq 0.2 $2 1);do
 		if [ $(echo "$x2 > 2*$x1" | bc) -eq 1 ];then
 			octave --silent --eval "replace($x1,$x2,$fstep)"
 			xline=$( cat xstr.txt )
-			echo Calc for x1=$x1, x2=$x2, x-coords: $xline
+			echo "          " $x1 " " $x2 "   " $xline
 			sed "4s/@/$xline/" tlumik.txt > tlumik.grd
 			ElmerGrid 1 2 tlumik.grd > /dev/null
 			fline=$( cat fstr.txt )
