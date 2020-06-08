7b0a72d73e4e71972862ad9b19da18138daf9eaa
Alexander Stefani <al3xand3rst3fani@gmail.com>
2020-06-08 17:27:55 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,9 +4,6 @@ fstep=$3
 echo Freq vec from 100 to 500 Hz with step $fstep Hz
 cd tlumik
 rm -f *.vtu *.dat
-sed "1s/@/$fline/" case.txt > case.sif
-number=$( echo "400/$fstep + 1" | bc)
-sed -i "15s/@/$number/" case.sif
 cd ..
 for x1 in $(seq 0.05 $1 0.4);do
 	for x2 in $(seq 0.2 $2 1);do
@@ -18,6 +15,9 @@ for x1 in $(seq 0.05 $1 0.4);do
 			ElmerGrid 1 2 tlumik.grd > /dev/null
 			fline=$( cat fstr.txt )
 			cd tlumik
+			sed "1s/@/$fline/" case.txt > case.sif
+			number=$( echo "400/$fstep + 1" | bc)
+			sed -i "15s/@/$number/" case.sif
 			ElmerSolver > /dev/null
 			cd ..
 		fi
