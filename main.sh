b2a3483b80380d2e26f277c3cd1600845ccfa3b6
Alexander Stefani <al3xand3rst3fani@gmail.com>
2020-06-08 22:55:48 +0200

    Freq vec improvement

--- main.sh
+++ main.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
+# przygotuj wektor czestotliwosci:
 fstep=$3
 echo "Freq vec:  [100 :" $fstep ": 500] Hz"
-rm -f tlumik/*.vtu tlumik/*.dat
+freq=()
+for i in $(seq 100 $fstep 500); do freq+=($i); done
+fstr=$( IFS=$' '; echo "${freq[*]}" )
+sed "1s/@/$fstr/" tlumik/case.txt > tlumik/case.sif
+number=$( echo "400/$fstep + 1" | bc)
+sed -i "15s/@/$number/" tlumik/case.sif
+rm -f tlumik/*.dat plaski/*.dat
+cp tlumik/case.sif plaski/case.sif
+# wykonaj plan eksperymentu: 
 echo "Calc for:   x1  |  x2  |  x-coords"
 for x1 in $(seq 0.05 $1 0.4);do
 	for x2 in $(seq 0.2 $2 1);do
 		if [ $(echo "$x2 > 2*$x1" | bc) -eq 1 ];then
-			octave --silent --eval "replace($x1,$x2,$fstep)"
+			# obliczenia dla tlumika:
+			octave --silent --eval "replace($x1,$x2)"
 			xline=$( cat xstr.txt )
 			echo "          " $x1 " " $x2 "   " $xline
 			sed "4s/@/$xline/" tlumik.txt > tlumik.grd
 			ElmerGrid 1 2 tlumik.grd > /dev/null
-			fline=$( cat fstr.txt )
 			cd tlumik
-			sed "1s/@/$fline/" case.txt > case.sif
-			number=$( echo "400/$fstep + 1" | bc)
-			sed -i "15s/@/$number/" case.sif
+			ElmerSolver > /dev/null
+			cd ..
+			# obliczenia dla plaskiego falowodu:
+			length=$(echo "-$x2-3*$x1" | bc) #zaleznie czy bedzie jeszcze +5m
+			sed "4s/@/$length/" plaski.txt > plaski.grd
+			ElmerGrid 1 2 plaski.grd > /dev/null
+			cd plaski
 			ElmerSolver > /dev/null
 			cd ..
 		fi
--- replace.m
+++ replace.m
@@ -1,4 +1,4 @@
-function replace(x1,x2,fstep)
+function replace(x1,x2)
 	warning('off', 'all');
 	xvec = [-x2-3*x1, -x2-2*x1, -x2-x1, -3*x1, -2*x1, 0, 0.1];
 	xstr = [];
@@ -7,13 +7,5 @@ function replace(x1,x2,fstep)
 	end
 	fid=fopen("xstr.txt",'w');
 	fprintf(fid, xstr);
-	fclose(fid);
-	fvec = 100:fstep:500;
-	fstr = [];
-	for i = fvec
-		fstr = [fstr, ' ', num2str(i)];
-	end
-	fid=fopen("fstr.txt",'w');
-	fprintf(fid, fstr);
 	fclose(fid);
 end
