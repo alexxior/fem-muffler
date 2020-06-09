393eaa2bb63b74844463d85d29de2aa0162bfba2
root <root@WINDELL-16PLB8U.localdomain>
2020-06-09 20:14:54 +0200

    Repo cleaning

--- data/FunkcjaCelu.m
+++ data/FunkcjaCelu.m
@@ -1,3 +1,4 @@
+% wyznacz srednia amplitude plaskiego falowodu
 filename = './plaski/modified.txt';
 M = dlmread(filename,',');
 w = size(M);
@@ -6,7 +7,7 @@ for i = [2,7,12,17]
     pbez = pbez + sqrt( M(:,i).^2 + M(:,i+1).^2 );
 end
 pbez = pbez/4;
-
+% wyznacz srednia amplitude tlumika
 filename = './tlumik/modified.txt';
 M = dlmread(filename,',');
 w = size(M);
@@ -15,6 +16,7 @@ for i = [2,7,12,17]
     ptlum = ptlum + sqrt( M(:,i).^2 + M(:,i+1).^2 );
 end
 ptlum = ptlum/4;
+% wyznacz strate wtracenia
 pstrat2 = pbez.^2 - ptlum.^2;
 %IL_mocy = 
 IL = 20*log10(pbez./ptlum);
@@ -24,4 +26,5 @@ IL_sort = zeros(number,w/number);
 for i = 1:w/number
     IL_sort(1:number,i) = IL(1+(i-1)*number : i*number);
 end
-disp(IL_sort);
+format bank
+disp([(100:fstep:500)', IL_sort]);
--- main.sh
+++ main.sh
@@ -6,7 +6,7 @@ echo "Freq vec:  [100 :" $fstep ": 500] Hz"
 freq=()
 for i in $(seq 100 $fstep 500);do freq+=($i);done
 fstr=$(IFS=$' '; echo "${freq[*]}")
-sed "1s/@/$fstr/" case.txt > tlumik/case.sif
+sed "1s/@/$fstr/" data/case.txt > tlumik/case.sif
 number=$(echo "400/$fstep + 1" | bc)
 sed -i "15s/@/$number/" tlumik/case.sif
 rm -f tlumik/*.dat plaski/*.dat xvec.txt
@@ -25,17 +25,17 @@ for x1 in $(seq 0.05 $1 0.4);do
 	for x2 in $(seq 0.2 $2 1);do
 		if [ $(echo "$x2 > 2*$x1" | bc) -eq 1 ];then
 			# obliczenia dla tlumika:
-			xline=$(octave --silent --eval "replace($x1,$x2)")
+			xline=$(octave --silent --eval "data/replace($x1,$x2)")
 			# echo $x1 $x2 >> xvec.txt
 			echo "          " $x1 "|" $x2 "| " $xline
-			sed "4s/@/$xline/" tlumik.txt > tlumik.grd
+			sed "4s/@/$xline/" data/tlumik.txt > tlumik.grd
 			ElmerGrid 1 2 tlumik.grd > /dev/null
 			cd tlumik
 			ElmerSolver > /dev/null
 			cd ..
 			# obliczenia dla plaskiego falowodu:
 			length=$(echo "-$x2-3*$x1" | bc) #zaleznie czy bedzie jeszcze +5m
-			sed "4s/@/$length/" plaski.txt > plaski.grd
+			sed "4s/@/$length/" data/plaski.txt > plaski.grd
 			ElmerGrid 1 2 plaski.grd > /dev/null
 			cd plaski
 			ElmerSolver > /dev/null
@@ -45,4 +45,4 @@ for x1 in $(seq 0.05 $1 0.4);do
 done
 sed -e 's/\s\+/,/g' tlumik/WyjscieTlumik.dat > tlumik/modified.txt
 sed -e 's/\s\+/,/g' plaski/WyjscieTlumik.dat > plaski/modified.txt
-octave FunkcjaCelu.m
+octave data/FunkcjaCelu.m
